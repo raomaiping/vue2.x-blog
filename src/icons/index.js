8419c8f0775ec17339de3e93dec05e321ad03efb
raomaiping <2582395486@qq.com>
2022-04-03 15:51:15 +0800

    创建 server 分支

--- src/icons/index.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-import SvgIcon from './SvgIcon.vue'// svg 组件
-// 全局注册
-Vue.component('svg-icon', SvgIcon)
-
-const req = require.context('./svg', false, /\.svg$/)
-const requireAll = requireContext => requireContext.keys().map(requireContext)
-requireAll(req)
--- src/layout/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as PageNav } from './PageNav'
-export { default as PageHeader } from './PageHeader'
-export { default as PageFooter } from './PageFooter'
